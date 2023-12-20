a69aef2a3ee02fab2579722ec339b26c3b796c43
Sato <cecile.hentsch.pro@gmail.com>
2023-12-20 09:18:03 +0100

    add speackers managing class

--- Entity/Speaker.php
+++ Entity/Speaker.php
@@ -3,17 +3,115 @@
 namespace Entity;
 
 class Speaker {
-    private string $name;
+    private int $id;
+    private string $firstName;
+    private string $lastName;
+    private string $mail;
+    /**
+     * @var module[]
+     */
+    private array $modules = [];
 
-    public function getName(): string
+
+
+    public function getId(): int
     {
-        return $this->name;
+        return $this->id;
     }
 
-    public function setName(string $name): self
+    public function getFirstName(): string
     {
-        $this->name = $name;
+        return $this->firstName;
+    }
+
+    public function getLastName(): string
+    {
+        return $this->lastName;
+    }
+
+     public function getMail(): string
+    {
+        return $this->mail;
+    }
+
+    /**
+     * @return Module[]
+     */
+    public function getModules(): array
+    {
+        return $this->modules;
+    }
+
+
+
+
+    public function setId(string $id): self
+    {
+       $this->id = $this->isOnlyNumericCharacters($id) ? ucfirst(strtolower($id)) : null;
 
         return $this;
+    }
+
+    public function setFirstName(string $firstName): self
+    {
+        $this->firstName = $this->isOnlyAlphabeticCharacters($firstName) ? ucfirst(strtolower($firstName)) : null;
+
+        return $this;
+    }
+
+    public function setLastName(string $lastName): self
+    {
+        $this->lastName = $this->isOnlyAlphabeticCharacters($lastName) ? ucfirst(strtolower($lastName)) : null;
+
+        return $this;
+    }
+
+    public function setMail(string $mail): self
+    {
+        $this->mail = $this->isOnlyAlphabeticCharacters($mail) ? ucfirst(strtolower($mail)) : null;
+
+        return $this;
+    }
+
+    /**
+     * @param Module[] $modules
+     */
+    public function setModules(array $modules): self
+    {
+        $this->modules = $modules;
+
+        return $this;
+    }
+
+    public function addModule(Module $module): self
+    {
+        // TODO : Vérifier que le module n'existe pas deja dans le tableau avant de l'ajouter
+        $this->modules[] = $module;
+
+        return $this;
+    }
+
+
+    //* Format check method
+    public function isOnlyAlphabeticCharacters($stringToCheck) :bool
+    {
+        $regex = '/^[a-zA-ZÀ-ÖØ-öø-ÿ\s]+$/u';
+
+        if (!preg_match($regex, $stringToCheck)) {
+            echo "Erreur dans la saisie merci de recommencer";
+            exit;
+        } 
+        return true;
+    }
+
+    public function isOnlyNumericCharacters($stringToCheck): bool
+    {
+        $regex = '/^[0-9]+$/';
+
+        if (!preg_match($regex, $stringToCheck)) {
+            echo "Erreur dans la saisie merci de recommencer";
+            exit;
+        } 
+        return true;
     }
 }
--- Repository/SpeakerRepository.php
+++ Repository/SpeakerRepository.php
@@ -11,6 +11,17 @@ class SpeakerRepository
     {
         $this->database = Database::getInstance();
     }
+    public function getList(): array
+    {
+        return $this->database
+                    ->executeQuery("SELECT m.moduleId as module_moduleId, m.name as module_name, m.durationModuleInHours as module_durationModuleInHours,
+                                    ms.speakerId as modulespeaker_speakerId, ms.moduleId as modulespeaker_moduleId,
+                                    s.speakerId as speaker_speakerId, s.firstName as speaker_firstName, s.lastName as speaker_lastName, s.mail as speaker_mail, s.password as speaker_password
+                                    FROM `speaker` s
+                                    JOIN modulespeaker ms ON m.moduleId = ms.moduleId
+                                    JOIN module m ON m.moduleId = ms.moduleId;")
+                    ->fetchAll();
+    }
 
     public function getList(): array
     {
