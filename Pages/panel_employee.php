75b00f25e145928a9b2243861c04821eacaef8d1
Sato <cecile.hentsch.pro@gmail.com>
2023-12-21 14:02:33 +0100

    visual set for create student

--- Pages/panel_employee.php
+++ Pages/panel_employee.php
@@ -1,12 +1,15 @@
 <?php require_once(__DIR__ . "\..\\Layouts\\header.php") ?>
 
-<div class="container bg-lightGrey">
-    <h2 class="text-black mb-5 text-center">Panneau de gestion</h2>
-    <div class="flex">promotion_creation
-        <section class="flex flex-col gap-2"> 
+<div>
+    <section class="w-full h-[300px] banner-bg bg-cover flex flex-col justify-center items-center mb-8 ">
+        <h1 class="text-center mt-8">Panneau de gestion</h1> 
+    </section>
+    <div class="flex">
+        <section class="flex flex-col gap-2 ml-4"> 
             <a class="bg-white rounded-md px-4 py-1" href="<?= $_SERVER['SCRIPT_NAME'] ?>?action=student_creation">Ajouter un étudiant</a>
             <a class="bg-white rounded-md px-4 py-1" href="<?= $_SERVER['SCRIPT_NAME'] ?>?action=formation_creation_form">Créer une formation</a>
             <a class="bg-white rounded-md px-4 py-1" href="<?= $_SERVER['SCRIPT_NAME'] ?>?action=module_creation_form">Créer un module</a>
+            <a class="bg-white rounded-md px-4 py-1" href="<?= $_SERVER['SCRIPT_NAME'] ?>?action=student_creation">Crée une promotion</a>
             <a class="bg-white rounded-md px-4 py-1" href="<?= $_SERVER['SCRIPT_NAME'] ?>?action=formation_delete_form">Supprimer une formation</a>
             <a class="bg-white rounded-md px-4 py-1" href="<?= $_SERVER['SCRIPT_NAME'] ?>?action=module_delete_form">Supprimer un module</a>
             <a class="bg-white rounded-md px-4 py-1" href="<?= $_SERVER['SCRIPT_NAME'] ?>?action=formation_list_all">Liste des formations</a>
