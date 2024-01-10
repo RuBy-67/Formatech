0f6310f1a778fae622219dad2b17f94556f26493
RuBy-67 <beyer.ruben67@gmail.com>
2024-01-10 19:38:11 +0100

    idem compatibilité OS

--- Autoloader.php
+++ Autoloader.php
@@ -1,41 +1,39 @@
 <?php
 class Autoloader
 {
-
     public static function register()
     {
-        spl_autoload_register(['Autoloader','autoload']); 
-    
+        spl_autoload_register(['Autoloader', 'autoload']);
     }
 
     public static function autoload($nom_classe)
     {
-        if( strpos($nom_classe,'DB\\') === 0 ){
+        if (strpos($nom_classe, 'DB\\') === 0) {
             // Seules les classes du namespace \DB seront chargées par l'autoload
-            $nom_classe = str_replace('DB\\','',$nom_classe);
+            $nom_classe = str_replace('DB\\', '', $nom_classe);
 
-            require_once(__DIR__ . "\\Db\\{$nom_classe}.php");
+            require_once(__DIR__ . "/Db/{$nom_classe}.php");
         }
 
-        if( strpos($nom_classe,'Entity\\') === 0 ){
+        if (strpos($nom_classe, 'Entity\\') === 0) {
             // Seules les classes du namespace \Structure seront chargées par l'autoload
-            $nom_classe = str_replace('Entity\\','',$nom_classe);
+            $nom_classe = str_replace('Entity\\', '', $nom_classe);
 
-            require_once(__DIR__ . "\\Entity\\{$nom_classe}.php");
+            require_once(__DIR__ . "/Entity/{$nom_classe}.php");
         }
 
-        if( strpos($nom_classe,'Repository\\') === 0 ){
+        if (strpos($nom_classe, 'Repository\\') === 0) {
             // Seules les classes du namespace \Structure seront chargées par l'autoload
-            $nom_classe = str_replace('Repository\\','',$nom_classe);
+            $nom_classe = str_replace('Repository\\', '', $nom_classe);
 
-            require_once(__DIR__ . "\\Repository\\{$nom_classe}.php");
+            require_once(__DIR__ . "/Repository/{$nom_classe}.php");
         }
 
-         if( strpos($nom_classe,'Mapper\\') === 0 ){
+        if (strpos($nom_classe, 'Mapper\\') === 0) {
             // Seules les classes du namespace \Structure seront chargées par l'autoload
-            $nom_classe = str_replace('Mapper\\','',$nom_classe);
+            $nom_classe = str_replace('Mapper\\', '', $nom_classe);
 
-            require_once(__DIR__ . "\\Mapper\\{$nom_classe}.php");
+            require_once(__DIR__ . "/Mapper/{$nom_classe}.php");
         }
     }
 }
