6e2f0a8ed43ac37f258d47e017f3369367dd1094
RuBy-67 <beyer.ruben67@gmail.com>
2024-04-06 17:12:42 +0200

    baseSQL corrigé

--- InitScript/formatechfutur.sql
+++ InitScript/formatechfutur.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le : ven. 22 déc. 2023 à 08:53
+-- Généré le : sam. 06 avr. 2024 à 15:11
 -- Version du serveur : 8.2.0
 -- Version de PHP : 8.2.13
 
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de données : `formatechfutur`
+-- Base de données : `formatech`
 --
 
 -- --------------------------------------------------------
@@ -34,7 +34,20 @@ CREATE TABLE IF NOT EXISTS `classroom` (
   `name` varchar(255) DEFAULT NULL,
   `capacityMax` int DEFAULT NULL,
   PRIMARY KEY (`classRoomId`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Déchargement des données de la table `classroom`
+--
+
+INSERT INTO `classroom` (`classRoomId`, `building`, `name`, `capacityMax`) VALUES
+(1, '1', 'Tesla', 10),
+(2, '1', 'Hawking', 15),
+(3, '1', 'Lovelace', 20),
+(4, '1', 'Amphithéâtre Boris Mallick', 200),
+(5, '2', 'Schrödinger', 25),
+(6, '2', 'Bohr', 25),
+(7, '2', 'Laboratoire Marie Curie', 50);
 
 -- --------------------------------------------------------
 
@@ -51,7 +64,7 @@ CREATE TABLE IF NOT EXISTS `employees` (
   `mail` varchar(255) DEFAULT NULL,
   `password` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`employeeId`)
-) ENGINE=MyISAM AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Déchargement des données de la table `employees`
@@ -61,7 +74,10 @@ INSERT INTO `employees` (`employeeId`, `firstName`, `lastName`, `job`, `mail`, `
 (3, 'Cécile', 'Hentsch', 'Administrateur', 'Ce@gmail.com', '$2y$10$ijP08cAJSWUPprxCRDNWzehtAqDIpaQjikLDiSKXsoILGpgn2dGsi'),
 (2, 'Sato', 'Test', 'Tester', 'Email@gmail.com', '$2y$10$yOZ6yoJb2wuYtsVTkYtIBOIhA4R.NCk6CJ9x/MSp8wdgKZ84dH3/i'),
 (5, 'Cécile', 'Hentsch', 'Administrateur', 'Cece@gmail.com', '$2y$10$RSe9q8AhAKiJI40MmIbxBeZnFGJk46el87D3Q1/VZ86TW5JigI3ym'),
-(7, 'Employee', 'Employee', 'Tester', 'Employee@employee.fr', '$2y$10$PpMmOP.Bhjs6eAF0Y8E15.NIs576NuiZmaLRzrXBkdDuqY2Eiy/2G');
+(7, 'Employee', 'Employee', 'Tester', 'Employee@employee.fr', '$2y$10$PpMmOP.Bhjs6eAF0Y8E15.NIs576NuiZmaLRzrXBkdDuqY2Eiy/2G'),
+(8, 'Adrien', 'Laparte', 'Admin', 'Adrien.laparte@formatech.fr', '$2y$10$7HhQb4Ohh/gudzK4.euAjuI.FQrr57XmYirFY/s/kM5vd8v0.kNiO'),
+(9, 'Ruben', 'Ruben', 'Tech', 'Ruben@employe.com', '$2y$10$INNPBOQgq4W37aOPxDPj8Oxwacp1keHYWqbtaSjidQVc8YlayFd7i'),
+(11, 'Test', 'Test', 'Tech', 'Test@gmail.com', '$2y$10$DDQs9sBKqpxawAp3745Zg.0t1LEmAdF53mcv6m3Ho1Wwd2sZ00hRO');
 
 -- --------------------------------------------------------
 
@@ -78,24 +94,17 @@ CREATE TABLE IF NOT EXISTS `formation` (
   `rncpLvl` int DEFAULT NULL,
   `accessibility` tinyint(1) DEFAULT NULL,
   PRIMARY KEY (`formationId`)
-) ENGINE=MyISAM AUTO_INCREMENT=36 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=42 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Déchargement des données de la table `formation`
 --
 
 INSERT INTO `formation` (`formationId`, `name`, `durationFormationInMonth`, `abbreviation`, `rncpLvl`, `accessibility`) VALUES
-(21, 'Sio', 4, 'Sio', 4, 0),
-(22, 'Test', 6, 'Tst', 7, 0),
-(25, 'Testtodelete', 5, 'Tdt', 5, 0),
-(26, 'Test', 5, 'Tst', 4, 0),
-(27, 'Testss', 5, 'Tstss', 6, 0),
-(30, 'Ta mère', 4, 'Tst', 4, 1),
-(31, 'Ta mère', 5, 'Td', 4, 1),
-(32, 'La robotique spaciale', 18, 'Rs', 7, 1),
-(33, 'Réalité virtuelle et réalité augmentée ', 18, 'Rvra', 7, 1),
-(34, 'Développement', 8, 'Dev', 4, 1),
-(35, 'Informatique quantique ', 14, 'Iq', 7, 1);
+(1, 'Technicien en maintenance robotique', 24, 'Tmr', 5, 1),
+(2, 'Bachelor administrateur en technologies novatrices', 12, 'BATN', 6, 1),
+(3, 'Ingénieur expert en fusion cybernétique', 24, 'Iefc', 7, 0),
+(39, 'Ingenieur informaticien', 7, 'Stl', 7, 1);
 
 -- --------------------------------------------------------
 
@@ -109,19 +118,23 @@ CREATE TABLE IF NOT EXISTS `module` (
   `name` varchar(255) DEFAULT NULL,
   `durationModuleInHours` int DEFAULT NULL,
   PRIMARY KEY (`moduleId`)
-) ENGINE=MyISAM AUTO_INCREMENT=22 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=34 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Déchargement des données de la table `module`
 --
 
 INSERT INTO `module` (`moduleId`, `name`, `durationModuleInHours`) VALUES
-(20, 'Test', 6),
-(2, 'Français', 6),
-(19, 'Jnb', 2),
-(3, 'Devweb', 6),
-(21, 'Encore un module', 8),
-(14, 'Tema le cours', 4);
+(22, 'Découverte de la robotique', 70),
+(23, 'Programmation assistée par IA', 98),
+(24, 'Conception robotique', 105),
+(25, 'Concepteur de réalités alternatives', 140),
+(26, 'Développement d\'intelligences articielles', 70),
+(27, 'Informatique quantique', 105),
+(28, 'Développement d\'énergies nouvelles et expérimentales', 140),
+(29, 'Ingénierie spatiale', 1119),
+(30, 'Mélanchon', 3),
+(31, 'Mélanchon', 1);
 
 -- --------------------------------------------------------
 
@@ -142,34 +155,13 @@ CREATE TABLE IF NOT EXISTS `moduleformation` (
 --
 
 INSERT INTO `moduleformation` (`moduleId`, `formationId`) VALUES
-(14, 30),
-(19, 31),
-(14, 21),
-(3, 21),
-(3, 30),
-(2, 22),
-(14, 26),
-(3, 26),
-(14, 25),
-(3, 25),
-(3, 28),
-(14, 28),
-(19, 1),
-(19, 21),
-(3, 31),
-(19, 32),
-(3, 32),
-(14, 32),
-(3, 33),
-(14, 33),
-(3, 34),
-(19, 35),
-(3, 35),
-(2, 21),
-(20, 4),
-(20, 21),
-(21, 1),
-(21, 21);
+(1, 1),
+(29, 39),
+(30, 2),
+(28, 1),
+(22, 3),
+(31, 2),
+(28, 39);
 
 -- --------------------------------------------------------
 
@@ -190,13 +182,8 @@ CREATE TABLE IF NOT EXISTS `modulespeaker` (
 --
 
 INSERT INTO `modulespeaker` (`speakerId`, `moduleId`) VALUES
-(2, 2),
-(4, 3),
-(2, 11),
-(4, 14),
-(2, 14),
-(4, 19),
-(4, 20);
+(2, 30),
+(2, 31);
 
 -- --------------------------------------------------------
 
@@ -213,14 +200,14 @@ CREATE TABLE IF NOT EXISTS `promotion` (
   `endingDate` date DEFAULT NULL,
   PRIMARY KEY (`promotionId`),
   KEY `formationId` (`formationId`)
-) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Déchargement des données de la table `promotion`
 --
 
 INSERT INTO `promotion` (`promotionId`, `formationId`, `promotionYears`, `startingDate`, `endingDate`) VALUES
-(2, 21, 2025, '2023-12-20', '2023-12-22');
+(3, 1, 2029, '2023-12-25', '2024-03-24');
 
 -- --------------------------------------------------------
 
@@ -243,15 +230,18 @@ CREATE TABLE IF NOT EXISTS `session` (
   KEY `promotionId` (`promotionId`),
   KEY `classRoomId` (`classRoomId`),
   KEY `speakerId` (`speakerId`)
-) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Déchargement des données de la table `session`
 --
 
 INSERT INTO `session` (`sessionId`, `date`, `startTime`, `endTime`, `moduleId`, `promotionId`, `classRoomId`, `speakerId`) VALUES
-(1, '2023-12-24', '07:33:00', '09:36:00', 2, 2, 0, 10),
-(2, '2023-12-06', '02:30:00', '04:30:00', 20, 2, 0, 10);
+(2, '2023-12-16', '07:30:00', '04:30:00', 25, 3, 0, 10),
+(4, '2023-12-24', '15:54:00', '16:54:00', 24, 3, 0, 2),
+(5, '2024-01-12', '03:28:00', '03:28:00', 25, 3, 0, 2),
+(6, '2024-02-02', '23:33:00', '04:03:00', 29, 3, 0, 2),
+(7, '2024-04-13', '18:54:00', '19:54:00', 24, 3, 0, 10);
 
 -- --------------------------------------------------------
 
@@ -267,16 +257,17 @@ CREATE TABLE IF NOT EXISTS `speaker` (
   `mail` varchar(255) DEFAULT NULL,
   `password` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`speakerId`)
-) ENGINE=MyISAM AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Déchargement des données de la table `speaker`
 --
 
 INSERT INTO `speaker` (`speakerId`, `firstName`, `lastName`, `mail`, `password`) VALUES
-(2, 'Cécile', 'Hentsch', 'Cecile.hentsch@gmail.com', '$2y$10$WmVMBrUfezMZrUH5Hqmtku1npeFwG.pWHmgBbhmRK8uflWtPPJfbC'),
-(4, 'Jesus', 'Christ', 'jesus@holy.com', 'mdp'),
-(10, 'Speaker', 'Speaker', 'Speaker@speaker.fr', '$2y$10$7xqTLgQ60SmA.GubVaqaQeg1Km3F8db3MEk2302174ZyoJ/W/CAsO');
+(2, 'Ruben', 'Hentsch', 'Cecile.hentsch@gmail.com', '$2y$10$CsDLNN7TKXmeNk0AIQUja.V9Pedzi0yFTML63gyJhVEwG2YIuJdE2'),
+(10, 'Speaker', 'Speaker', 'Speaker@speaker.fr', '$2y$10$7xqTLgQ60SmA.GubVaqaQeg1Km3F8db3MEk2302174ZyoJ/W/CAsO'),
+(11, 'Adrien', 'Laporte', 'Adrien.laporte@formatech.fr', '$2y$10$C.GOuWf4CvS/dwjMScbXpeh7bm6wM6NLaZIRMm0oCWZDRwwrzyiqG'),
+(13, 'Clémentine', 'Trouez', 'Intervenant@intervenant.com', '$2y$10$yMvTPLqIMsHbRGE7ix51L.ArCz.1ZJ2iorc0zyIg6xcoI4Au5wS3y');
 
 -- --------------------------------------------------------
 
@@ -293,16 +284,19 @@ CREATE TABLE IF NOT EXISTS `student` (
   `birthDate` date DEFAULT NULL,
   `password` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`studentId`)
-) ENGINE=MyISAM AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Déchargement des données de la table `student`
 --
 
 INSERT INTO `student` (`studentId`, `firstName`, `lastName`, `mail`, `birthDate`, `password`) VALUES
-(4, 'Clément', 'Gangloff', 'Cgangloff@bird.eu', '2000-09-18', '$2y$10$Zbprq6xcfIxI7lcYpRVkL.SKEaaqY8WAcxtfZ9tpCXZ66HekjlncO'),
+(4, 'Clément', 'Gangloff', 'Cgangloff@bird.eu', '2000-09-18', '$2y$10$ZEcQ53ikXFYU2r7FO2Qro.w9STK74WEAGYFHyvCDITE.R8yUm/hsm'),
 (3, 'Cécile', 'Hentsch', 'Email@gmail.com', '2023-12-15', '$2y$10$NxilPQ2OqxIt9AIHgNizHuJdmlmGNXuT6P/JNADEJns0ohJpiwSQi'),
-(5, 'Student', 'Student', 'Student@student.fr', '2023-12-22', '$2y$10$cRCnsiVanv7jxHiMJvBaWOWcLfWKSSIMQqy6C60bX.zLExiKl5Ch6');
+(5, 'Student', 'Student', 'Student@student.fr', '2023-12-22', '$2y$10$cRCnsiVanv7jxHiMJvBaWOWcLfWKSSIMQqy6C60bX.zLExiKl5Ch6'),
+(8, 'Ruben', 'Beyer', 'Employee@employee.fr', '2024-04-13', '$2y$10$iQW44LcKrRNMaKj2O0RaZuzpNNDkqbukcrobqeAJG9GeH.FFNkH6K'),
+(7, 'Lucas', 'Romain', 'Lucas.romain@formatech.fr', '2023-12-23', '$2y$10$A4x8jN/sYdWIJ8ke4EvADObjyWvQWIgGn3QKg/ThRkz29d9Ms//Sq'),
+(9, 'Test', 'Test', 'Student@student.fr', '2024-04-18', '$2y$10$T2YqkWmY6N/kPwe2DT19Y.wMdrT7URUgOYLkNzXyNuv0SlJKDTc/S');
 
 -- --------------------------------------------------------
 
@@ -323,9 +317,10 @@ CREATE TABLE IF NOT EXISTS `studentpromotion` (
 --
 
 INSERT INTO `studentpromotion` (`studentId`, `promotionId`) VALUES
-(4, 2),
-(3, 2),
-(5, 2);
+(5, 3),
+(7, 3),
+(8, 3),
+(9, 3);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
