17b0cabaf8a4a475ef9197de9652f81a743a17d6
RuBy-67 <beyer.ruben67@gmail.com>
2024-04-07 13:59:02 +0200

    .

--- Pages/login/connexion.php
+++ Pages/login/connexion.php
@@ -77,7 +77,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             }
         
         } else {
-            echo "Aucun utilisateur correspondant sur " . $type . ".";
+            echo "erreur mdp " . $type . ".";
         }
     }
 }
